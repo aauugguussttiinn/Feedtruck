1df1275e3a91944d4adddc501ff99a1b20cd9080
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-06-15 18:43:59 +0200

    controller and views setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :foodtrucks
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "items#new"
+
+  namespace :customer do
+    resources :customers
+    resource :customers do 
+      resources :orders
+    end
+  end
+
+  namespace :foodtruck do
+    resources :foodtrucks
+    resource :foodtrucks do 
+      resources :items 
+      resources :orders
+    end
+  end
 end
