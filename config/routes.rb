596002b790e8dffd350c338ff75772538a0083f5
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-06-16 12:06:54 +0200

    removed namespace from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,11 @@ Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "items#new"
+  root "foodtruck#index"
 
-  namespace :customer do
-    resources :customers
-    resource :customers do 
-      resources :orders
-    end
-  end
-
-  namespace :foodtruck do
-    resources :foodtrucks
-    resource :foodtrucks do 
-      resources :items 
-      resources :orders
-    end
+  resources :orders, except: [:destroy]
+  resource :customer
+  resources :foodtrucks do
+    resources :items
   end
 end
