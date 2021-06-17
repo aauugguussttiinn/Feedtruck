722c8c421a631aefba859e6743466324987bc573
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-06-17 08:16:04 +0100

    fixing bug

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -59,6 +59,6 @@ class CustomersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def customer_params
-      params.require(:customer).permit(:email, :password)
+      params.require(:customer).permit(:email, :password, :location_id, :first_name, :last_name)
     end
 end
