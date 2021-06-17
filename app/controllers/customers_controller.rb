69d65bab02fb2a4bbafdf96906857bfd4afce551
guinus70 <guinus70@hotmail.fr>
2021-06-17 14:12:33 +0200

    translation of forms + customer edit page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -54,7 +54,7 @@ class CustomersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_customer
-      @customer = Customer.find(params[:id])
+      @customer = current_customer
     end
 
     # Only allow a list of trusted parameters through.
