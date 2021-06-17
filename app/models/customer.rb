40998f5b1e3fef42dca38049675ec32cfd99e10a
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-06-17 10:45:42 +0200

    add cart after create user + can add items to cart

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+  after_create :add_cart
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +8,8 @@ class Customer < ApplicationRecord
   belongs_to :location
   has_many :orders
   has_one :cart
+
+  def add_cart
+    self.cart = Cart.create(customer: self)
+  end
 end
