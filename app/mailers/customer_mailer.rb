38ab7c4de037f2b5890576ed518b9958450b0706
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-06-17 11:14:04 +0100

    create functions to use in the mailer and wrote proper content for the emails

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -3,13 +3,16 @@ class CustomerMailer < ApplicationMailer
 
   def welcome_email(customer)
     @customer = customer  
-    @url = 'https://feedtruck.herokuapp.com/'
+    @url = 'https://feedtruck.herokuapp.com/customers/sign_in'
     mail(to: @customer.email, subject: 'Bienvenue sur FeedTruck')
   end
   
   def order_email(customer)
     @customer = customer
     @order = @customer.orders.last
+    @foodtruck = @order.foodtruck
+    @total_price = @order.total_price
+    @order_contents = @order.order_contents
     @url = 'https://feedtruck.herokuapp.com/'
     mail(to: @customer.email, subject: 'Confirmation de votre commande')
   end
