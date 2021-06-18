9e867a022b076d533e059273890c994884897fa2
guinus70 <guinus70@hotmail.fr>
2021-06-18 11:07:29 +0200

    Adding some links in the footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#homepage"
+  get 'team', to: 'static_pages#team'
+  get 'contact', to:'static_pages#contact'
 
   devise_for :customers
 
