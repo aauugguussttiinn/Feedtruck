18417f16ad1521cbc6364bc12cde4d69b2e4a728
guinus70 <guinus70@hotmail.fr>
2021-06-21 21:08:17 +0200

    add the foodtruck dashboard view

--- app/controllers/myfoodtruck/dashboards_controller.rb
+++ app/controllers/myfoodtruck/dashboards_controller.rb
@@ -2,6 +2,8 @@ class Myfoodtruck::DashboardsController < ApplicationController
   
   # GET /foodtruck/dashboard
   def show
+    @id = current_myfoodtruck_foodtruck.id
+    @orders = Order.where(foodtruck_id: @id).all
   end
 
 end
