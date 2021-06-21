1e6aa0dea97a15791b32f71ea376cbc4a82232f4
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-06-21 16:55:33 +0200

    merge

--- app/controllers/myfoodtruck/foodtrucks_controller.rb
+++ app/controllers/myfoodtruck/foodtrucks_controller.rb
@@ -1,69 +1,36 @@
 class Myfoodtruck::FoodtrucksController < ApplicationController
   before_action :set_foodtruck_foodtruck, only: %i[ show edit update destroy ]
 
-  # GET /foodtruck/foodtrucks or /foodtruck/foodtrucks.json
-  def index
-    @foodtruck_foodtrucks = Foodtruck::Foodtruck.all
-  end
-
   # GET /foodtruck/foodtrucks/1 or /foodtruck/foodtrucks/1.json
   def show
-  end
-
-  # GET /foodtruck/foodtrucks/new
-  def new
-    @foodtruck_foodtruck = Foodtruck::Foodtruck.new
   end
 
   # GET /foodtruck/foodtrucks/1/edit
   def edit
   end
 
-  # POST /foodtruck/foodtrucks or /foodtruck/foodtrucks.json
-  def create
-    @foodtruck_foodtruck = Foodtruck::Foodtruck.new(foodtruck_foodtruck_params)
-
-    respond_to do |format|
-      if @foodtruck_foodtruck.save
-        format.html { redirect_to @foodtruck_foodtruck, notice: "Foodtruck was successfully created." }
-        format.json { render :show, status: :created, location: @foodtruck_foodtruck }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @foodtruck_foodtruck.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # PATCH/PUT /foodtruck/foodtrucks/1 or /foodtruck/foodtrucks/1.json
   def update
     respond_to do |format|
-      if @foodtruck_foodtruck.update(foodtruck_foodtruck_params)
-        format.html { redirect_to @foodtruck_foodtruck, notice: "Foodtruck was successfully updated." }
-        format.json { render :show, status: :ok, location: @foodtruck_foodtruck }
+      if @myfoodtruck_foodtruck.update(myfoodtruck_foodtruck_params)
+        format.html { redirect_to @myfoodtruck_foodtruck, notice: "Foodtruck was successfully updated." }
+        format.json { render :show, status: :ok, location: @myfoodtruck_foodtruck }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @foodtruck_foodtruck.errors, status: :unprocessable_entity }
+        format.json { render json: @myfoodtruck_foodtruck.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /foodtruck/foodtrucks/1 or /foodtruck/foodtrucks/1.json
-  def destroy
-    @foodtruck_foodtruck.destroy
-    respond_to do |format|
-      format.html { redirect_to foodtruck_foodtrucks_url, notice: "Foodtruck was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_foodtruck_foodtruck
-      @foodtruck_foodtruck = Foodtruck::Foodtruck.find(params[:id])
+      @myfoodtruck_foodtruck = Myfoodtruck::Foodtruck.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def foodtruck_foodtruck_params
-      params.fetch(:foodtruck_foodtruck, {})
+      params.fetch(:myfoodtruck_foodtruck, {})
     end
 end
--- spec/helpers/myfoodtruck/registrations_helper_spec.rb
+++ spec/helpers/myfoodtruck/registrations_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the Foodtruck::SessionsHelper. For example:
+# the Foodtruck::RegistrationsHelper. For example:
 #
-# describe Foodtruck::SessionsHelper do
+# describe Foodtruck::RegistrationsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe Foodtruck::SessionsHelper, type: :helper do
+RSpec.describe Myfoodtruck::RegistrationsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
