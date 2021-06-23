bfd7afb14b83566bf853bebaf3212a920fc075ff
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-06-23 15:36:30 +0100

    implemented the vote on the foodtruck page: added the different cases such as location missing, or last vote is less than 24h

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -4,4 +4,12 @@ module CustomersHelper
     current_customer.location_id != nil ? true : false
   end
   
+  def voted_once?
+    current_customer.votes.count > 0 ? true : false
+  end
+
+  def last_vote_over_24?
+    voted_once? && Time.now > current_customer.votes.last.updated_at + 86400 ? true : false
+  end
+  
 end
