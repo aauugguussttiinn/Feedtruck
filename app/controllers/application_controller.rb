87dcc28ab234d74eeaef29c7a435d87bea33ae67
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-06-25 15:36:55 +0200

    signin and up as customer redicts to ft#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+
+  def after_sign_in_path_for(customer)
+    flash[:notice] = 'Connecté en tant que Gourmet'
+    foodtrucks_path
+  end
 end
--- app/controllers/myfoodtruck/registrations_controller.rb
+++ app/controllers/myfoodtruck/registrations_controller.rb
@@ -2,7 +2,7 @@ class Myfoodtruck::RegistrationsController < Devise::RegistrationsController
   before_action :not_logged_in?, only:  %i[ new]
 
   def after_sign_up_path_for(foodtruck)
-    flash[:notice] = 'Bienvenue! Vous êtes bien connecté en tant que Foodtruck'
-    foodtrucks_path
+    flash[:notice] = 'Bienvenue! Renseignez votre profil pour continuer'
+    myfoodtruck_foodtruck_path
   end
 end
