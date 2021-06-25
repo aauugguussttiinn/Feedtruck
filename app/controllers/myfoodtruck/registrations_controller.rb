8cac4bc3af456aa17c4012f1c337942d40220a04
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-06-25 15:16:12 +0200

    login as ft redirects to dashb and signup as ft redirects to ft#index

--- app/controllers/myfoodtruck/registrations_controller.rb
+++ app/controllers/myfoodtruck/registrations_controller.rb
@@ -1,3 +1,8 @@
 class Myfoodtruck::RegistrationsController < Devise::RegistrationsController
   before_action :not_logged_in?, only:  %i[ new]
+
+  def after_sign_up_path_for(foodtruck)
+    flash[:notice] = 'Bienvenue! Vous êtes bien connecté en tant que Foodtruck'
+    foodtrucks_path
+  end
 end
